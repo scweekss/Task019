c4b18ed189e078b32ec97241e38efeec71178dc8
scweekkkkk <124443824+scweekss@users.noreply.github.com>
2023-03-09 19:12:32 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+//14212 -> нет
+
+//12821 -> да
+
+//23432 -> да
+
+Console.WriteLine("Введите пятизначенное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int a1 = Convert.ToInt32(Console.ReadLine());
+int b1 = Convert.ToInt32(Console.ReadLine());
+if(a1 == b1)
+{
+    number = number / 10;
+    int a2 = number / 100;
+    int b2 = number % 10;
+    if(a2 == b2)
+     Console.WriteLine("Число яляется палиндромом");
+}
+   
+else
+{
+    Console.WriteLine("Число не яляется палиндромом");
+}
